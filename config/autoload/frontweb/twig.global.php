e5afa7a3c8967b19d7bec9815ff9ea4cf9514508
Mehmet Korkmaz <mehmet.korkmaz@reformo.net>
2019-11-01 00:33:59 +0300

    twig config options changed to use .env file

--- config/autoload/frontweb/twig.global.php
+++ config/autoload/frontweb/twig.global.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 return [
     'twig' => [
-        'cache_dir' => 'data/cache/twig',
-        'debug' => false,
+        'cache_dir' => getenv('ENABLE_TWIG_CACHE') == 'false' ? false : getenv('ENABLE_TWIG_CACHE'),
+        'debug' =>  filter_var(getenv('ENABLE_TWIG_DEBUG'), FILTER_VALIDATE_BOOLEAN),
         'optimizations' => -1,
         'auto_reload' => true
     ]
